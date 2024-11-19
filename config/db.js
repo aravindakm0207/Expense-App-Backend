dce96a13cdfa1b3448e7e02fc61b2b1e202decee
Aravinda <aravindakm0207@gmail.com>
2024-11-19 08:43:51 +0530

    cors configuration

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const configureDB = async () => {
     try {
-        
-        const db = await mongoose.connect('mongodb://127.0.0.1:27017/exp-july24');
+        //('mongodb://127.0.0.1:27017/exp-july24');
+        const db = await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected to DB");
     } catch (err) {
         console.log(err);
